d3f6cc1a177b4dd2226bc122356c4bbbd4b5f36a
EUNKYUNG JUNG <thxjek@gmail.com>
2021-10-23 22:48:56 +0900

    Update UserStorage model

--- src/app/src/routes/home/home.ctrl.js
+++ src/app/src/routes/home/home.ctrl.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const UserStorage = require("../../models/UserStorage");
 const output = {
     hello: (req, res) => {
         res.render("home/index"); // '/' 도메인으로왔을때, 이동할 수 있는 코드.
@@ -9,25 +10,26 @@ const output = {
     },
 };
 
-const users = {
-    id: ["woorimIT", "나개발", "김팀장"],
-    paword: ["1234", "1234", "123456"],
-};
-
 const process = {
     login: (req, res) => {
         // console.log(req.body);
         const id = req.body.id,
             psword = req.body.psword;
+        console.log(id, psword);
 
+        const users = UserStorage.getUsers("id", "psword");
+        const response = {};
         if (users.id.includes(id)) {
             const idx = users.id.indexOf(id);
             if (users.psword[idx] === psword) {
-                return res.json({
-                    success: true,
-                });
+                response.success = true;
+                return res.json();
             }
         }
+
+        response.success = false;
+        response.msg = "로그인에 실패하였습니다.";
+        return res.json(response);
     },
 };
 module.exports = {
