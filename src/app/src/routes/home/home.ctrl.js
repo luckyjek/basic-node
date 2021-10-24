6c15b1cd06b4ce0766f90dd14b75edeca447eeb4
EUNKYUNG JUNG <thxjek@gmail.com>
2021-10-24 13:36:09 +0900

    test

--- src/app/src/routes/home/home.ctrl.js
+++ src/app/src/routes/home/home.ctrl.js
@@ -12,24 +12,25 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        // console.log(req.body);
-        const id = req.body.id,
-            psword = req.body.psword;
-        console.log(id, psword);
-
-        const users = UserStorage.getUsers("id", "psword");
-        const response = {};
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.psword[idx] === psword) {
-                response.success = true;
-                return res.json();
-            }
-        }
-
-        response.success = false;
-        response.msg = "로그인에 실패하였습니다.";
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
+        // console.log(req.body);
+        // const id = req.body.id,
+        //     psword = req.body.psword;
+        // console.log(id, psword);
+        // const users = UserStorage.getUsers("id", "psword");
+        // const response = {};
+        // if (users.id.includes(id)) {
+        //     const idx = users.id.indexOf(id);
+        //     if (users.psword[idx] === psword) {
+        //         response.success = true;
+        //         return res.json();
+        //     }
+        // }
+        // response.success = false;
+        // response.msg = "로그인에 실패하였습니다.";
+        // return res.json(response);
     },
 };
 module.exports = {
